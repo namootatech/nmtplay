05ddeb480f528a0a0fdf39901f7d5de767dd6ea6
Ayabonga Qwabi <aya@simply.co.za>
2024-12-09 15:07:12 +0200

    lungisa i footer

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -179,8 +179,8 @@ const Layout = ({
       <main className='text-gray-700 bg-gray-900'>
         <div>{children}</div>
         <section class='py-24 bg-gray-900'>
-          <div class='max-w-7xl mx-auto px-5 sm:px-10 md:px-12 lg:px-5'>
-            <div class='w-full relative py-8 md:py-10 px-6 md:px-8 rounded-2xl bg-gradient-to-tr from-gray-100 to-gray-200 dark:from-gray-900'>
+          <div class='max-w-7xl mx-auto px-5 sm:px-10 md:px-12 lg:px-5 '>
+            <div class='w-full text-white relative py-8 md:py-10 px-6 md:px-8 rounded-2xl bg-gradient-to-tr from-gray-500 to-gray-900 dark:from-gray-900'>
               <div class='absolute right-0 top-0 h-full w-full flex justify-end'>
                 <div class='w-28 h-28 overflow-hidden flex rounded-xl relative blur-2xl'>
                   <span class='absolute w-16 h-16 -top-1 -right-1 bg-fuchsia-500 rounded-md rotate-45'></span>
@@ -197,14 +197,14 @@ const Layout = ({
                 </div>
               </div>
               <div class='mx-auto text-center max-w-xl md:max-w-2xl relative space-y-8'>
-                <h1 class='text-3xl/tight sm:text-4xl/tight md:text-5xl/tight font-bold text-fuchsia-950 dark:text-white'>
+                <h1 class='text-3xl/tight sm:text-4xl/tight md:text-5xl/tight font-bold text-fuchsia-150 dark:text-white'>
                   Le website ngesNgesi sithi yi
                   <span class='text-transparent bg-clip-text bg-gradient-to-br from-fuchsia-600 from-20% via-indigo-400 via-30% to-teal-600'>
                     File Sharing website
                   </span>{' '}
                   for abantu base Eastern Cape
                 </h1>
-                <p class='text-gray-700 dark:text-gray-300'>
+                <p class='text-gray-100 dark:text-gray-300'>
                   Ayika gqitywa ntozakuthi kodwa Lena yi website eyenzelwe abo
                   bathetha ulwimi lwesiXhosa ingakumbi abo base Eastern Cape
                   province yakweli lase South Africa. Sizama ukwakha isizwe
