8f7d0209fb92ee949953d55894bf0941e4a753ea
Ayabonga Qwabi <aya@simply.co.za>
2024-12-09 14:49:19 +0200

    utshintsho: faka u next-sitemap for uba sigenerate automatically ii sitemaps

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  siteUrl: process.env.SITE_URL || 'http://localhost:3000', // Your website's URL
+  generateRobotsTxt: true, // (Optional) Generates a robots.txt file
+  sitemapSize: 7000, // Number of URLs per sitemap file
+};
