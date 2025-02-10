ffef636142a39214ed264e33ad88111e907946d5
Ayabonga Qwabi <ayabongaqwabi@gmail.com>
2025-02-10 03:42:58 +0200

    add adstera

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,6 +1,8 @@
 // components/Layout.js
 import Head from 'next/head';
 import { NavBar } from './nav';
+import { Ban } from 'lucide-react';
+import Banner from '../Banner';
 
 const Layout = ({
   title = 'NMTPlay - Download Free Files',
@@ -222,7 +224,7 @@ const Layout = ({
             </div>
           </div>
         </section>
-
+        <Banner />
         <footer class='bg-white rounded-lg shadow m-4 dark:bg-gray-800'>
           <div class='w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between'>
             <span class='text-sm text-gray-500 sm:text-center dark:text-gray-400'>
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -18,6 +18,7 @@ export default function App({ Component, pageProps }) {
       <PageLayout>
         <Banner />
         <Component {...pageProps} />
+        <Banner />
       </PageLayout>
     </AuthProvider>
   );
