b2615d963c6b8714c1d8be1806d82dbff5d2d627
Ayabonga Qwabi <ayabongaqwabi@gmail.com>
2025-02-10 08:08:58 +0200

    add googles adsense

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang='en'>
-      <Head />
+      <Head>
+        <script
+          async
+          src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8294995671791919'
+          crossorigin='anonymous'
+        ></script>
+      </Head>
 
       <body className='antialiased'>
         <Main />
