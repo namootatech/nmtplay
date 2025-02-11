300a42127720737a28edf0202484c80df37177dd
Ayabonga Qwabi <ayabongaqwabi@gmail.com>
2025-02-11 11:42:05 +0200

    add adnsense ver

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,6 +4,10 @@ export default function Document() {
   return (
     <Html lang='en'>
       <Head>
+        <meta
+          name='google-adsense-account'
+          content='ca-pub-8294995671791919'
+        ></meta>
         <script
           async
           src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8294995671791919'
