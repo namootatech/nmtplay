233dcae23cb35e5bc5a44bd737fab007ee5afe69
Matthew Fourie <mattfourie3@gmail.com>
2025-02-14 00:23:27 +0200

    Removed the sw.js file and added the code insert in the head tags for verification with propellerads.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -19,7 +19,10 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
+        {/* PopCash details for verification */}
         <meta name="ppck-ver" content="91377b8d6f3e95025cc9d9733176c7bb" />
+        {/* Monetag/PropellerAds details for verification */}
+        <meta name="monetag" content="910c78db187be2cf1e11a5cd3d1ecfb6"></meta>
       </Head>
       <AuthProvider>
         <PageLayout>
